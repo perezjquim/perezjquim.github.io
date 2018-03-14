aaa97b1f2b868a7eb31138627de8b291d2a41b88
Joaquim Perez <jquim1@hotmail.com>
2018-03-14 15:19:33 +0000

    tests

--- repos.js
+++ repos.js
@@ -3,6 +3,6 @@ $(document).ready(function()
 	$(".nav-default a").click(function()
 	{
 		var url = "https://perezjquim.github.io/"+$(this).attr('href').substr(1);
-		$(".modal-body").load("<iframe width="100%" height="100%" frameborder="0" scrolling="no" allowtransparency="true" src="'+url+'"></iframe>'");
+		$(".modal-body").html("<iframe width="100%" height="100%" frameborder="0" scrolling="no" allowtransparency="true" src="'+url+'"></iframe>'");
 	});
 });

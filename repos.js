54269a0d10e463fd8e69261435d3e1c3b8a62bc6
Joaquim Perez <jquim1@hotmail.com>
2018-03-14 15:48:21 +0000

    Cleanup

--- repos.js
+++ repos.js
@@ -2,10 +2,8 @@ $(document).ready(function()
 {
 	$(".nav-default a").click(function()
 	{
-		//var url = "https://perezjquim.github.io/"+$(this).attr('href').substr(1);
-		//$(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="no" allowtransparency="true" src="'+url+'"></iframe>');
-		
 		BootstrapDialog.show({
+		  title: '',
             message: function(dialog) {
                 var $message = $('<div></div>');
                 var pageToLoad = dialog.getData('pageToLoad');

56b828a5647cf27fa3ebc369a4852dcc4f9818aa
Joaquim Perez <jquim1@hotmail.com>
2018-06-10 22:42:28 +0100

    Major cleanup

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
-$(document).ready(function()
+function chooseTab(page)
 {
-    choose($(".nav-tabs a:first"));
-    $(".nav-tabs a").click(function()
+	var name = page.attr('href').substr(1);
+
+	var content = $("#content");
+	content.empty();
+
+	if(name === "external" || name === "home")
+	{
+		content.load(name+".html");
+	}
+  else
+  {
+  	content.append("<ul class='nav nav-default'>");
+
+  	var repos = tabs
+  		.filter((t) => t.name === name)[0]
+  		.repos;
+  	repos.forEach((repo) =>
+  	{
+  		$('.nav-default').append('<li><a href="#'+repo.reponame+'">'+repo.name+'</a></li>'); 
+  	});
+
+  	content.append("</ul>");
+
+    $(".nav-default a").off("click").click(function()
     {
-	   $(this).tab('show');
-	   choose($(".nav-tabs .active > a"));
+    	showRepo($(this));
+    });      	
+  }
+}
+
+function showRepo(repo)
+{
+  BootstrapDialog.show(
+  {
+      title: 'Repository',
+      message: function(dialog) 
+      {
+          var body = $('<div></div>');
+          var html = dialog.getData('repo');
+          body.load(html);
+          return body;
+      },
+      data: 
+      {
+          'repo': "https://perezjquim.github.io/"+repo.attr('href').substr(1)
+      }
+  });
+}
+
+$(document).ready(function()
+{	
+		tabs.forEach((tab)=>
+		{
+			$("#tabs").append("<li><a href='#"+tab.name+"'>"+tab.name+"</a></li>");
+		});
+
+    $("#tabs a").off("click").click(function()
+    {
+    	$(this).tab('show');
+			chooseTab($(this));
     });
-    function choose(p)
-    {
-        var pagename = p.attr('href').substr(1);
-	   $("#content").load(pagename+".html");
-    }
+
+   	chooseTab($(".nav-tabs a:first"));
 });

b04bfdf0473f8c000731b9d85ec19391154da91d
Joaquim Perez <jquim1@hotmail.com>
2019-02-19 23:00:23 +0000

    cleanup / webslides -> reveal.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 $(document).ready(function()
 {
-	tabs.forEach((tab)=>
-	{
-		tab.repos.forEach((repo) => 
-		{
-			$("#webslides").append(
-				"<section>"+
-					"<div class='wrap'>"+
-						"<div class='grid vertical-align'>"+
-							"<div class='column'>"+
-								"<h3><strong>"+tab.name+"</strong></h3>"+
-								"<p class='text-intro'>"+repo.name+"</p>"+
-							"</div>"+
-							"<div class='column'>"+
-								"<pre><iframe src='https://perezjquim.github.io/"+repo.reponame+"/'></iframe></pre>"+
-							"</div>"+
-						"</div>");
-		});
-	});
-
-      window.ws = new WebSlides();	
+        tabs.forEach((tab) =>
+        {
+                tab.repos.forEach((repo) =>
+                {
+                        $(".slides").append("<section>" + "<div>" + "<div>" + "<h3><strong>" + tab.name + "</strong></h3>" + "<p class='text-intro'>" + repo.name + "</p>" + "</div>" + "<div>" + "<pre><iframe src='https://perezjquim.github.io/" + repo.reponame + "/'></iframe></pre>" + "</div>" + "</section>");
+                });
+        });
+        /*Reveal.configure(
+        {
+                autoSlide: 5000
+        });*/
+        Reveal.initialize(
+        {
+                controls: true,
+                progress: true,
+                history: true,
+                center: true,
+                transition: 'slide'
+        });
 });

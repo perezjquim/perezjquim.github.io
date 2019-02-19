f51b67a5658d2efbdfa1f1e0ebf8042e078ec944
Joaquim Perez <jquim1@hotmail.com>
2019-02-19 23:15:09 +0000

    css tweaks / autoslide enabled

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ $(document).ready(function()
                         $(".slides").append("<section>" + "<div>" + "<div>" + "<h3><strong>" + tab.name + "</strong></h3>" + "<p class='text-intro'>" + repo.name + "</p>" + "</div>" + "<div>" + "<pre><iframe src='https://perezjquim.github.io/" + repo.reponame + "/'></iframe></pre>" + "</div>" + "</section>");
                 });
         });
-        /*Reveal.configure(
+        Reveal.configure(
         {
                 autoSlide: 5000
-        });*/
+        });
         Reveal.initialize(
         {
                 controls: true,

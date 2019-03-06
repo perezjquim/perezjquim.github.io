e77289472b681b5be7fcaa6a1b95ef22c3650187
Joaquim Perez <jquim1@hotmail.com>
2019-03-06 22:21:52 +0000

    homepage binding (1)

--- js/index.js
+++ js/index.js
@@ -3,13 +3,19 @@ $(document).ready(function()
         //
         // templates
         //
-        const template_li = '<li>' + '<div>' + '<svg class="@icon@">' + '<use xlink:href="#@icon@">' + '</use>' + '</svg>' + '@name@'
-        '</div>' + '</li>';
-        const template_section = "<section>" + "<div>" + "<div>" + "<h3><strong>@name@</strong></h3>" + "<p class='text-intro'>@label@</p>" + "</div>" + "<div>" + "<pre><iframe src='/@reponame@'></iframe></pre>" + "</div>" + "</section>";
+        const templates = {
+                "portfolio-item": '<li>' + '<div>' + '<svg class="@icon@">' + '<use xlink:href="#@icon@">' + '</use>' + '</svg>' + '@name@' + '</div>' + '</li>',
+                "repo-section": "<section>" + "<div>" + "<div>" + "<h3><strong>@name@</strong></h3>" + "<p class='text-intro'>@label@</p>" + "</div>" + "<div>" + "<pre><iframe src='/@reponame@'></iframe></pre>" + "</div>" + "</section>",
+                "home":
+                {
+                        "base": '<section home>' + '<img height="25%" src="lib/images/avatar.jpeg"/>' + '<h2>' + '<strong>' + '@title@' + '</strong>' + '</h2>' + '<p>' + '@subtitle@' + '</p>' + '<div social>' + '</div>' + '</section>',
+                        "social-item": +'<a href="@link@" title="@title@">' + '<svg class="@icon@">' + '<use xlink:href="#@icon@">' + '</use>' + '</svg>' + '</a>'
+                }
+        };
         //
         //
         //
-        function bindData(tabs)
+        function bindTabs(tabs)
         {
                 tabs.forEach((tab) =>
                 {
@@ -23,6 +29,18 @@ $(document).ready(function()
                 });
         }
 
+        function bindHome(home)
+        {
+                const templates = templates.home;
+                const oHome = templates.base.replace(/@title@/g, home.title).replace(/@subtitle@/g, home.subtitle);
+                $("[content]").append(oHome);
+                home.social.forEach((s) =>
+                {
+                        const oSocial = templates.social.replace(/@link@/g, s.link).replace(/@title@/g, s.title, /@icon@/g, s.icon);
+                        $("[home]").append(oSocial);
+                });
+        }
+
         function start()
         {
                 $.get("config/config.json", (config) =>
@@ -33,9 +51,13 @@ $(document).ready(function()
                         Reveal.initialize(oInitialize);
                 });
         }
-        $.get("config/tabs.json", (tabs) =>
+        $.get("config/home.json", (home) =>
         {
-                bindData(tabs);
-                start();
+                bindHome(home);
+                $.get("config/tabs.json", (tabs) =>
+                {
+                        bindTabs(tabs);
+                        start();
+                });
         });
 });

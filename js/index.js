ffc4356f7e7bc7b9fadaab7e57d5d6e8d60f6d0d
Joaquim Perez <jquim1@hotmail.com>
2019-03-06 20:04:26 +0000

    misc fixes / cleanup

--- js/index.js
+++ js/index.js
@@ -1,18 +1,30 @@
 $(document).ready(function()
 {
+        //
+        // templates
+        //
         const template_li = '<li>' + '<div>' + '<svg class="@icon@">' + '<use xlink:href="#@icon@">' + '</use>' + '</svg>' + '@name@'
         '</div>' + '</li>';
         const template_section = "<section>" + "<div>" + "<div>" + "<h3><strong>@name@</strong></h3>" + "<p class='text-intro'>@label@</p>" + "</div>" + "<div>" + "<pre><iframe src='/@reponame@'></iframe></pre>" + "</div>" + "</section>";
-        $.get("config/tabs.json", (tabs) =>
+        //
+        //
+        //
+        function bindData(tabs)
         {
                 tabs.forEach((tab) =>
                 {
-                        $("[portfolio-items]").append(template_li.replace("@icon@",tab.icon).replace("@name@",tab.name));
+                        const oItem = template_li.replace(/@icon@/g, tab.icon).replace(/@name@/g, tab.name);
+                        $("[portfolio-items]").append(oItem);
                         tab.repos.forEach((repo) =>
                         {
-                                $(".slides").append(template_section.replace("@name@",tab.name).replace("@label@", repo.label).replace("@reponame@",repo.reponame));
+                                const oSection = template_section.replace(/@name@/g, tab.name).replace(/@label@/g, repo.label).replace(/@reponame@/g, repo.reponame)
+                                $(".slides").append(oSection);
                         });
                 });
+        }
+
+        function start()
+        {
                 $.get("config/config.json", (config) =>
                 {
                         const oConfigure = config.configure;
@@ -20,5 +32,10 @@ $(document).ready(function()
                         Reveal.configure(oConfigure);
                         Reveal.initialize(oInitialize);
                 });
+        }
+        $.get("config/tabs.json", (tabs) =>
+        {
+                bindData(tabs);
+                start();
         });
 });

b62e466cbe6d905802cea4c353d5b1dbb1fc0092
Joaquim Perez <jquim1@hotmail.com>
2019-03-08 23:51:34 +0000

    cleanup

--- js/index.js
+++ js/index.js
@@ -1,1 +1,56 @@
-$(document).ready(function(){const e={"portfolio-item":'<li><div><svg class="@icon@"><use xlink:href="#@icon@"></use></svg>@name@</div></li>',"repo-section":"<section><div><div><h3><strong>@name@</strong></h3><p class='text-intro'>@label@</p></div><div><pre><iframe src='/@reponame@/'></iframe></pre></div></section>",home:{base:'<img src="lib/images/avatar.jpeg"/><h2><strong>@title@</strong></h2><p>@subtitle@</p>',"social-item":'<a href="@link@" title="@title@" style="background-color:@color@;"><svg class="@icon@"><use xlink:href="#@icon@"></use></svg></a>'}};$.get("config/home.json",i=>{!function(i){const o=e.home.base.replace(/@title@/g,i.title).replace(/@subtitle@/g,i.subtitle);$("[home]").prepend(o),i.social.forEach(i=>{const o=e.home["social-item"].replace(/@link@/g,i.link).replace(/@title@/g,i.title).replace(/@icon@/g,i.icon).replace(/@color@/g,i.color);$("[social]").append(o)})}(i),$.get("config/tabs.json",i=>{!function(i){i.forEach(i=>{const o=e["portfolio-item"].replace(/@icon@/g,i.icon).replace(/@name@/g,i.name);$("[portfolio-items]").append(o),i.repos.forEach(o=>{const n=e["repo-section"].replace(/@name@/g,i.name).replace(/@label@/g,o.label).replace(/@reponame@/g,o.reponame);$(".slides").append(n)})})}(i),$.get("config/config.json",e=>{const i=e.configure,o=e.initialize;Reveal.configure(i),Reveal.initialize(o)})})})});
+$(document).ready(function()
+{
+        const e = {
+                "portfolio-item": '<li><div><svg class="@icon@"><use xlink:href="#@icon@"></use></svg>@name@</div></li>',
+                "repo-section": "<section><div><div><h3><strong>@name@</strong></h3><p class='text-intro'>@label@</p></div><div><pre><iframe src='/@reponame@/'></iframe></pre></div></section>",
+                home:
+                {
+                        base: '<img src="lib/images/avatar.jpeg"/><h2><strong>@title@</strong></h2><p>@subtitle@</p>',
+                        "social-item": '<a href="@link@" title="@title@" style="background-color:@color@;"><svg class="@icon@"><use xlink:href="#@icon@"></use></svg></a>'
+                }
+        };
+        function bind(template,data)
+        {
+        	for(var key in data)
+        	{
+        		const regexp = new RegExp("@"+key+"@",'g');
+        		template = template.replace(regexp,data[key]);
+        	}
+        	return template;
+        }
+        $.get("config/home.json", i =>
+        {
+                ! function(i)
+                {
+                	const template = e.home.base;
+                        const o = bind(template,i);
+                        $("[home]").prepend(o), i.social.forEach(i =>
+                        {
+                        	const template = e.home["social-item"];
+                                const o = bind(template,i);
+                                $("[social]").append(o);
+                        })
+                }(i), $.get("config/tabs.json", i =>
+                {
+                        ! function(i)
+                        {
+                                i.forEach(i =>
+                                {
+                                	const template = e["portfolio-item"];
+                                        const o = bind(template,i);
+                                        $("[portfolio-items]").append(o), i.repos.forEach(o =>
+                                        {
+                                                const template = e["repo-section"];
+                                                const n = bind(template,i);
+                                                $(".slides").append(n)
+                                        })
+                                })
+                        }(i), $.get("config/config.json", e =>
+                        {
+                                const i = e.configure,
+                                        o = e.initialize;
+                                Reveal.configure(i), Reveal.initialize(o)
+                        })
+                })
+        })
+});

e355f6cf1fb37fa008c0f01e00e76448cf3eba5d
Joaquim Perez <jquim1@hotmail.com>
2021-07-31 00:00:43 +0100

    settimeout's added for performance

--- webapp/apps/generic_readme/controller/Home.controller.js
+++ webapp/apps/generic_readme/controller/Home.controller.js
@@ -9,22 +9,24 @@ sap.ui.define([
 		_onObjectMatched: function(oEvent) {
 			this.setBusy(true);
 
-			const oEmbed = this.byId("embed-repo");
-			const oStartupModel = this.getModel("startup");
-			const sRepoLabel = oStartupModel.getProperty("/repo_label")[0];
-			const sRepoName = oStartupModel.getProperty("/repo_name")[0];
-			oEmbed.setContent(`<embed src='https://perezjquim.github.io/${sRepoName}' width='100%' height='100%'/>`);
+			setTimeout(function() {
+				const oEmbed = this.byId("embed-repo");
+				const oStartupModel = this.getModel("startup");
+				const sRepoLabel = oStartupModel.getProperty("/repo_label")[0];
+				const sRepoName = oStartupModel.getProperty("/repo_name")[0];
+				oEmbed.setContent(`<embed src='https://perezjquim.github.io/${sRepoName}' width='100%' height='100%'/>`);
 
-			const oComponent = this.getOwnerComponent();
-			oComponent.getService("ShellUIService").then(
-				function(oService) {
-					oService.setTitle(sRepoLabel);
-					this.setBusy(false);
-				}.bind(this),
-				function(oError) {
-					this.setBusy(false);
-				}.bind(this),
-			);
+				const oComponent = this.getOwnerComponent();
+				oComponent.getService("ShellUIService").then(
+					function(oService) {
+						oService.setTitle(sRepoLabel);
+						this.setBusy(false);
+					}.bind(this),
+					function(oError) {
+						this.setBusy(false);
+					}.bind(this),
+				);
+			}.bind(this));
 		}
 	});
 });

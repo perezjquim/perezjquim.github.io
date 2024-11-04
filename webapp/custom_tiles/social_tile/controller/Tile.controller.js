46b612516bcfa0b272434c436e2180868d609315
Joaquim Perez <jquim1@hotmail.com>
2024-11-04 20:33:00 +0000

    improv social tile error handling

--- webapp/custom_tiles/social_tile/controller/Tile.controller.js
+++ webapp/custom_tiles/social_tile/controller/Tile.controller.js
@@ -1,23 +1,21 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+], function (Controller) {
 	return Controller.extend("com.perezjquim.showcase.custom_tiles.social_tile.controller.Tile", {
-		
-		onAfterRendering: function(oEvent)
-		{
+
+		onAfterRendering: function (oEvent) {
 			const oSource = oEvent.getSource();
 			const oConfigModel = oSource.getModel("config");
 			const sBackground = oConfigModel.getProperty("/tile_background");
 
-			if(sBackground)
-			{			
+			if (sBackground) {
 				const oTile = oSource && oSource.getContent() && oSource.getContent()[0];
 				const oDomRef = oTile.getDomRef();
 				$(oDomRef).css("background", sBackground);
 			}
 		},
 
-		onTilePress: function(oEvent) {
+		onTilePress: function (oEvent) {
 			const oSource = oEvent.getSource();
 			const oConfigModel = oSource.getModel("config");
 
@@ -25,35 +23,32 @@ sap.ui.define([
 			window.open(sTargetUrl);
 		},
 
-		fetchCount: async function(sEndpoint, sProperty)
-		{
-			try
-			{
-				if(sEndpoint)
-				{
-					const oResponse = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(sEndpoint)}`);
-					if(oResponse.ok)
-					{
-						const oJSONResponse = await oResponse.json();
-						const sContents = oJSONResponse.contents;
-						if(sProperty)
-						{
-							const oContents = JSON.parse(sContents);
-							return sProperty.split('.').reduce(function(oPrev, sCurr) {
-								return oPrev ? oPrev[sCurr] : null
-							}, oContents)
-						}
-						else
-						{
-							const iValue = sContents;
-							return iValue;
+		fetchCount: async function (sEndpoint, sProperty) {
+			if (sEndpoint) {
+				var iNoOfTries = 5;
+				while (iNoOfTries > 0) {
+					try {
+						const oResponse = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(sEndpoint)}`);
+						if (oResponse.ok) {
+							const oJSONResponse = await oResponse.json();
+							const sContents = oJSONResponse.contents;
+							if (sProperty) {
+								const oContents = JSON.parse(sContents);
+								return sProperty.split('.').reduce(function (oPrev, sCurr) {
+									return oPrev ? oPrev[sCurr] : null
+								}, oContents)
+							}
+							else {
+								const iValue = sContents;
+								return iValue;
+							}
 						}
 					}
+					catch (oException) {
+						console.warn(oException);
+					}
+					iNoOfTries -= 1;
 				}
-			}
-			catch(oException)
-			{
-				console.warn(oException);
 			}
 		}
 
